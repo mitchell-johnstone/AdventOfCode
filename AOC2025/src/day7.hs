2f59d68f0d11916a3e6622c98fc13fd8b453cd18
Mitchell Johnstone <mitchell.johnstone@directsupply.com>
2025-12-13 22:56:59 -0600

    Fix day7

--- AOC2025/src/day7.hs
+++ AOC2025/src/day7.hs
@@ -1,29 +1,32 @@
 module Main (main) where
 
--- import Data.List (transpose)
--- import Data.List.Split (splitWhen)
-import Data.List (nub)
+import qualified Data.Map as Map
+
+data State = State {splitCount :: Int, histogram :: Map.Map (Int, Int) Int}
 
 main :: IO ()
 main = do
   contents <- readFile "input/day7.txt"
   let l = lines contents
-  print $ fst . (!! (length l)) $ iterate (emit (getSplits l) nub) (0,[getStart l])
-  print $ (+) 1 . fst . !! (length l) $ iterate (emit (getSplits l) id) (0,[getStart l])
+  let startState = State {splitCount = 0, histogram = Map.fromList [(getStart l, 1)]}
+  -- iterate till 1 past the end, to make sure we capture all splits
+  let endState = (!! (1 + length l)) $ iterate (emit (getSplits l)) startState
+  print $ splitCount endState
+  print $ sum . Map.elems $ histogram endState
 
-emit :: [(Int,Int)] -> ([(Int,Int)] -> [(Int,Int)]) -> (Int, [(Int,Int)]) -> (Int, [(Int,Int)])
-emit splits postprocess (cnt, fronteir) = (cnt', fronteir')
+emit :: [(Int,Int)] -> State -> State
+emit splits (State cnt hist) = (State cnt' hist')
   where
-    inF = filter (`elem` splits) $ moveDown fronteir
-    notInF = filter (not . (`elem` splits)) $ moveDown fronteir
-    fronteir' = postprocess $ notInF ++ horizontal inF
-    cnt' = cnt + length inF
+    (split, stay) = Map.partitionWithKey (\k _ -> elem k splits) hist
+    cnt' = cnt + (length $ Map.keys split)
+    hist' = Map.mapKeys moveDown $ Map.unionsWith (+) [(Map.mapKeys moveLeft split), stay, (Map.mapKeys moveRight split)]
 
-moveDown :: [(Int, Int)] -> [(Int, Int)]
-moveDown = map (\(a,b) -> (a+1,b))
+move :: (Int, Int) -> (Int, Int) -> (Int, Int)
+move (r',c') (r,c) = (r+r',c+c')
 
-horizontal :: [(Int, Int)] -> [(Int, Int)]
-horizontal v = (map (\(a,b) -> (a,b+1)) v) ++ (map (\(a,b) -> (a,b-1)) v)
+moveDown = move (1,0)
+moveLeft = move (0,-1)
+moveRight = move (0,1)
 
 getStart :: [String] -> (Int, Int)
 getStart = (,) 0 . length . fst . break (=='S') . head
@@ -35,15 +38,3 @@ getSplits g =
   , (j,cell) <- zip [0..] row
   , cell == '^'
   ]
-
--- replace :: Char -> Char -> String -> String
--- replace c t = map (\cur -> if cur == c then t else cur)
---
--- emit :: String -> String -> String
--- emit ls rs = split $ zipWith (\l r -> if l == '|' then (if r == '^' then r else l) else '.') ls rs
---
--- split :: String -> String
--- split s = map toRay $ zipWith3 or3 (False:(init $ map (== '^') s)) ((tail $ map (== '^') s)++[False]) (map (== '|') s)
---   where
---     toRay = \b -> if b then '|' else '.'
---     or3 = \a b c -> a || b || c
